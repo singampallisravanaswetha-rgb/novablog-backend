2459dae02c54b7afca8ec45037df09e610bb74e8
Singamapalli Venkata Sravana Swetha <singampallisravanaswetha@gmail.com>
2026-06-04 16:10:29 +0530

    Fix CORS for Vercel

--- server.js
+++ server.js
@@ -17,7 +17,10 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://novablog-frontend.vercel.app",
+    ],
     credentials: true,
   })
 );
